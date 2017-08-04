47ad16b35da523d206a13fa1292558a1c450323d
ipluswave <ipluswebstar@mail.com>
2017-08-04 05:40:03 +0200

    auto attribute assigners add/edit page completed

--- app/controllers/admin/auto_attributes_assigners_controller.rb
+++ app/controllers/admin/auto_attributes_assigners_controller.rb
@@ -1,11 +1,84 @@
 class Admin::AutoAttributesAssignersController < Admin::AdminBaseController
 
   def create
-  
+    @new_assigner = AutoAttributesAssigner.new
+    @new_assigner[:status]                        = params[:auto_attributes_assigner][:status]
+    @new_assigner[:priority]                      = params[:auto_attributes_assigner][:priority]
+    @new_assigner[:title_contains]                = params[:auto_attributes_assigner][:title_contains]
+    @new_assigner[:title_doesnot_contains]        = params[:auto_attributes_assigner][:title_doesnot_contains]
+    @new_assigner[:description_contains]          = params[:auto_attributes_assigner][:description_contains]
+    @new_assigner[:description_doesnot_contains]  = params[:auto_attributes_assigner][:description_doesnot_contains]
+    @new_assigner[:filter_by_price_from]          = params[:auto_attributes_assigner][:filter_by_price_from]
+    @new_assigner[:filter_by_price_to]            = params[:auto_attributes_assigner][:filter_by_price_to]
+
+    if !params[:filter_category].nil?
+      @new_assigner[:filter_category]            = params[:filter_category].join(',')
+    end
+
+    if !params[:assign_attribute_category].nil?
+      @new_assigner[:assign_attribute_category]  = params[:assign_attribute_category].join(',')
+    end
+
+    assign_attribute_options = []
+
+    if !params[:custom_fields].nil?
+      params[:custom_fields].each do |key, value|
+
+        if params[:overwrite_property].nil? || params[:overwrite_property][key].nil?
+          assign_attribute_options += [key, value, 'no']
+        else
+          assign_attribute_options += [key, value, 'yes']
+        end
+      end
+    end
+    
+    @new_assigner[:assign_attribute_options] = assign_attribute_options.join(',')
+
+    @new_assigner.save
+
+    flash[:notice] = 'Successfully Created!'
+    redirect_to :action => :new
   end
 
   def update
+    @new_assigner = @auto_attributes_assigner = AutoAttributesAssigner.find(params[:id])
 
+    @new_assigner[:status]                        = params[:auto_attributes_assigner][:status]
+    @new_assigner[:priority]                      = params[:auto_attributes_assigner][:priority]
+    @new_assigner[:title_contains]                = params[:auto_attributes_assigner][:title_contains]
+    @new_assigner[:title_doesnot_contains]        = params[:auto_attributes_assigner][:title_doesnot_contains]
+    @new_assigner[:description_contains]          = params[:auto_attributes_assigner][:description_contains]
+    @new_assigner[:description_doesnot_contains]  = params[:auto_attributes_assigner][:description_doesnot_contains]
+    @new_assigner[:filter_by_price_from]          = params[:auto_attributes_assigner][:filter_by_price_from]
+    @new_assigner[:filter_by_price_to]            = params[:auto_attributes_assigner][:filter_by_price_to]
+
+    if !params[:filter_category].nil?
+      @new_assigner[:filter_category]            = params[:filter_category].join(',')
+    end
+
+    if !params[:assign_attribute_category].nil?
+      @new_assigner[:assign_attribute_category]  = params[:assign_attribute_category].join(',')
+    end
+
+    assign_attribute_options = []
+
+    if !params[:custom_fields].nil?
+      params[:custom_fields].each do |key, value|
+
+        if params[:overwrite_property].nil? || params[:overwrite_property][key].nil?
+          assign_attribute_options += [key, value, 'no']
+        else
+          assign_attribute_options += [key, value, 'yes']
+        end
+      end
+    end
+    
+    @new_assigner[:assign_attribute_options] = assign_attribute_options.join(',')
+
+    @new_assigner.save
+
+    flash[:notice] = 'Successfully Saved!'
+    redirect_to :action => :index
   end
 
   def index
@@ -20,12 +93,24 @@ class Admin::AutoAttributesAssignersController < Admin::AdminBaseController
   def new
     @body_class_name         = "admin auto_attributes_assigners_new"
     @selected_left_navi_link = "auto_attributes_assigner"
+
+    @auto_attributes_assigner = AutoAttributesAssigner.new
+    @custom_field_questions   = CustomField.all
+
+    @filter_category           = @auto_attributes_assigner.get_filter_categories
+    @assign_attribute_category = @auto_attributes_assigner.get_assign_attribute_categories
   
   end
 
   def edit
     @body_class_name         = "admin auto_attributes_assigners_edit"
     @selected_left_navi_link = "auto_attributes_assigner"
+    
+    @auto_attributes_assigner = AutoAttributesAssigner.find(params[:id])
+    @custom_field_questions   = CustomField.all
+
+    @filter_category           = @auto_attributes_assigner.get_filter_categories
+    @assign_attribute_category = @auto_attributes_assigner.get_assign_attribute_categories
   
   end
 
--- app/models/auto_attributes_assigner.rb
+++ app/models/auto_attributes_assigner.rb
@@ -29,7 +29,11 @@
 
 class AutoAttributesAssigner < ActiveRecord::Base
   def is_in_category(search_category_id)
-    categories = filter_category.split(',')
+    if assign_attribute_category.nil?
+      return false
+    end
+
+    categories = assign_attribute_category.split(',')
     search_category = Category.find_by_id(search_category_id)
 
     categories.each do |category_id|
@@ -42,6 +46,10 @@ class AutoAttributesAssigner < ActiveRecord::Base
   end
 
   def is_in_category_array(search_category_array)
+    if search_category_array.nil?
+      return false;
+    end
+
     search_category_array.each do |search_category_id|
     
       if is_in_category(search_category_id)
@@ -50,6 +58,87 @@ class AutoAttributesAssigner < ActiveRecord::Base
     end
 
     return false
+  end
+
+  def get_filter_categories
+    if filter_category.nil?
+      return []
+    end
+
+    filter_category.split(',')
+  end
+
+  def get_assign_attribute_categories
+    if assign_attribute_category.nil?
+      return []
+    end
+
+    assign_attribute_category.split(',')
+  end
+
+  def answer_for(custom_field)
+    if custom_field.nil? || custom_field.id.nil? || assign_attribute_options.nil?
+      return ''
+    end
+
+    # id, value, flag for overwrite,
+    options_array = assign_attribute_options.split(',')
+
+    if options_array.length%3 != 0
+      return ''
+    end
+
+    option_index = 0
+
+    while option_index < options_array.length do
+
+      if options_array[option_index] == custom_field.id.to_s
+        return options_array[option_index + 1]
+      end
+
+      option_index += 3
+    end
+
+    return ''
+
+  end
+
+  def overwrite_answer_for(custom_field)
+    if custom_field.nil? || custom_field.id.nil? || assign_attribute_options.nil?
+      return ''
+    end
+
+    # id, value, flag for overwrite,
+    options_array = assign_attribute_options.split(',')
+
+    if options_array.length%3 != 0
+      return ''
+    end
+
+    option_index = 0
+
+    while option_index < options_array.length do
+
+      if options_array[option_index] == custom_field.id.to_s
+        return options_array[option_index + 2] == 'yes'
+      end
+
+      option_index += 3
+    end
+
+    return ''
+
+  end
+
+  def product_ids
+    products = get_queued_products
+
+    product_ids = []
+    products.each do |product|
+      product_ids += products[:id]
+    end
+
+    product_ids = product_ids.join(',')
   end
 
   def get_queued_products
@@ -63,7 +152,12 @@ class AutoAttributesAssigner < ActiveRecord::Base
   	  return queued_products
   	end
 
-    categories = filter_category.split(',')
+    categories = []
+    queued_products = Listing
+
+    if !filter_category.nil?
+      categories = filter_category.split(',')
+    end
 
     query_where = []
 
@@ -72,11 +166,11 @@ class AutoAttributesAssigner < ActiveRecord::Base
       query_where += category.own_and_subcategory_ids
     end
 
-    if (!query_where.empty?)
+    if !query_where.empty?
       queued_products = Listing.where(:category_id => query_where)
     end
 
-    if (!title_contains.nil?)
+    if !title_contains.nil?
       titles = title_contains.split(',')
 
       titles.each do |search_word|
@@ -84,7 +178,7 @@ class AutoAttributesAssigner < ActiveRecord::Base
       end
     end
 
-    if (!title_doesnot_contains.nil?)
+    if !title_doesnot_contains.nil?
       titles = title_doesnot_contains.split(',')
 
       titles.each do |search_word|
@@ -93,7 +187,7 @@ class AutoAttributesAssigner < ActiveRecord::Base
     end
 
 
-    if (!description_contains.nil?)
+    if !description_contains.nil?
       descriptions = description_contains.split(',')
 
       descriptions.each do |search_word|
@@ -102,7 +196,7 @@ class AutoAttributesAssigner < ActiveRecord::Base
     end
 
 
-    if (!description_doesnot_contains.nil?)
+    if !description_doesnot_contains.nil?
       descriptions = description_doesnot_contains.split(',')
 
       descriptions.each do |search_word|
@@ -112,6 +206,10 @@ class AutoAttributesAssigner < ActiveRecord::Base
 
     if (!filter_by_price_from.nil?) && (!filter_by_price_to.nil?)
       queued_products = queued_products.where('price_cents >= ? AND price_cents <= ?', filter_by_price_from, filter_by_price_to)
+    end
+
+    if queued_products == Listing
+      queued_products = []
     end
 
     queued_products
